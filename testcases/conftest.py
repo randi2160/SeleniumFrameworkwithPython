c28a50dda7f554f0965bf2bfa55ba10f23bfa02c
khemlall mangal <randi2160@yahoo.com>
2022-05-21 19:08:45 -0400

    adding driver manager code to manage browser exe automatically

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -1,19 +1,25 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
 
 
 @pytest.fixture()
 def setup(browser):
     if browser == 'chrome':
-        driver = webdriver.Chrome(executable_path='C:\\Users\\User\\PycharmProjects\\PytestHybridframework\\utilites\\chromedriver.exe')
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        #driver = webdriver.Chrome(executable_path='C:\\Users\\User\\PycharmProjects\\PytestHybridframework\\utilites\\chromedriver.exe')
         print("Launching chrome browser.........")
     elif browser == 'firefox':
-        driver = webdriver.Firefox()
+        driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+        #driver = webdriver.Firefox()
         print("Launching firefox browser.........")
     else:
-        driver = driver=webdriver.Chrome(executable_path='C:\\Users\\User\\PycharmProjects\\PytestHybridframework\\utilites\\chromedriver.exe')
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        #driver = driver=webdriver.Chrome(executable_path='C:\\Users\\User\\PycharmProjects\\PytestHybridframework\\utilites\\chromedriver.exe')
     return driver
 def pytest_addoption(parser):    # This will get the value from CLI /hooks
     parser.addoption("--browser")
